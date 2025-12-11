f4f3d60020aeaf7023dacade936f34a0a94f3e01
Atul Himanshu <priyanshuprogrammer49@gmail.com>
2025-12-11 22:25:04 +0530

    server is running

--- server.js
+++ server.js
@@ -30,16 +30,20 @@ app.use(
 // app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoutes);
 app.use("/api", healthRouter);
+// app.use("/", return "hello");
+app.use("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
-  connectDB();
+//   connectDB();
 });
